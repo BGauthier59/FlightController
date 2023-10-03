dd92876dbe09732eb1199085bd0547a92777ebe6
Aurélien <71558277+Arcaane@users.noreply.github.com>
2023-10-03 15:01:48 +0200

    Start Controller

---
+++ Flight/Assets/PlayerTestController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTestController : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private Vector2 xAxis;
+    [SerializeField] private Vector2 zAxis;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        
+    }
+}
