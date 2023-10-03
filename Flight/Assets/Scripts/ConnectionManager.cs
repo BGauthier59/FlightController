8fbd52a4991b71dedd74844a88e708f580ebf2c5
BGauthier59 <gauthierbaudinet@gmail.com>
2023-10-03 14:25:14 +0200

    lobby v1

---
+++ Flight/Assets/Scripts/ConnectionManager.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ConnectionManager : MonoSingleton<ConnectionManager>
+{
+    [SerializeField] private Transform[] spawnZones;
+    [SerializeField] private float startHoldTime;
+
+    [SerializeField] private List<PlayerController> players;
+    
+    public void OnPlayerJoined(PlayerInput input)
+    {
+        int number = PlayerInputManager.instance.playerCount - 1;
+        PlayerController player = input.GetComponent<PlayerController>();
+        player.OnJoined(spawnZones[number].position, number);
+        players.Add(player);
+
+        if (number == PlayerInputManager.instance.maxPlayerCount - 1)
+        {
+            AllPlayerJoined();
+        }
+    }
+
+    private void AllPlayerJoined()
+    {
+        Debug.Log("all player joined");
+        UIManager.instance.AllPlayerConnect();
+        foreach (var player in players)
+        {
+            player.SetReadyToStart();
+        }
+    }
+
+    public async void TryStartGame()
+    {
+        foreach (var pc in players)
+        {
+            if (!pc.IsHoldingComplete()) return;
+        }
+
+        // Starts Game
+        foreach (var pc in players)
+        {
+            pc.ExitLobby();
+        }
+
+        UIManager.instance.ExitLobbyGUI();
+        await PostProcessManager.instance.SwitchVolume(1, 1);
+        
+        Debug.Log("ok");
+    }
+}
---
+++ Flight/Assets/Scripts/UIManager.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEditor.VersionControl;
+using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+using Task = System.Threading.Tasks.Task;
+
+public class UIManager : MonoSingleton<UIManager>
+{
+    [Serializable]
+    public struct PlayerDataGUI
+    {
+        public TextMeshProUGUI state;
+        public Transform icon;
+        public Image readyGauge;
+    }
+
+    [SerializeField] private string waitingText, connectedText;
+
+    [SerializeField] private PlayerDataGUI[] playerData;
+    [SerializeField] private GameObject readyZone;
+
+    [SerializeField] private Animation anim;
+    [SerializeField] private AnimationClip exitLobbyClip;
+    
+    private void Start()
+    {
+        readyZone.SetActive(false);
+        foreach (var data in playerData)
+        {
+            data.state.text = waitingText;
+            data.icon.localScale = Vector3.zero;
+        }
+    }
+
+    public async void PlayerConnect(int index)
+    {
+        PlayerDataGUI data = playerData[index];
+        float timer = 0;
+        Vector3 zero = Vector3.zero;
+        Vector3 one = Vector3.one;
+                
+        data.state.text = connectedText;
+
+        while (timer < .5f)
+        {
+            data.icon.localScale = Vector3.Lerp(zero, one, timer / .5f);
+            await Task.Yield();
+            timer += Time.deltaTime;
+        }
+
+        data.icon.localScale = one;
+    }
+
+    public void AllPlayerConnect()
+    {
+        readyZone.SetActive(true);
+    }
+
+    public void RefreshReadyGaugeGUI(int index, float value)
+    {
+        PlayerDataGUI data = playerData[index];
+        data.readyGauge.fillAmount = value;
+    }
+
+    public void ExitLobbyGUI()
+    {
+        anim.clip = exitLobbyClip;
+        anim.Play();
+    }
+}
