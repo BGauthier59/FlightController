ced9a56e34cda53c4c1596c321f39adf81ca6aa4
Aurélien <71558277+Arcaane@users.noreply.github.com>
2023-10-03 17:01:16 +0200

    Debut de controller qui va virer

--- Flight/Assets/Scripts/PlayerController.cs
+++ Flight/Assets/Scripts/PlayerController.cs
@@ -10,11 +10,32 @@ public class PlayerController : MonoBehaviour
     private int index;
     private bool isWaitingToHold, isHolding;
     private float holdTimer;
+    
     [SerializeField] private float holdSpeed;
+    [SerializeField] private float jumpSpeed = 400f;
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private AnimationCurve dragCurve;
 
+    [SerializeField] private float playerSpeed;
+    [SerializeField] private float vAxis;
+    [SerializeField] private float hAxis;
+    [SerializeField] private float baseHSpeed = 10f;
+    [SerializeField] private float baseVSpeed = 10f;
+    
     private void Update()
     {
         if(isWaitingToHold) RefreshStartGauge(Time.deltaTime);
+
+        if (isWaitingToHold) return;
+        
+        // Position
+        transform.Translate(Vector3.forward * (playerSpeed * Time.deltaTime));
+        
+        // Rotation 
+        var hVelocity = baseHSpeed * vAxis * Time.deltaTime;
+        var vVelocity = baseVSpeed * hAxis * Time.deltaTime;
+        Quaternion rotation = Quaternion.Euler(vVelocity, hVelocity, 0);
+        rb.MoveRotation(rb.rotation * rotation);
     }
 
     public void OnJoined(Vector3 initPos, int index)
@@ -37,11 +58,20 @@ public class PlayerController : MonoBehaviour
 
     public void OnMove(InputAction.CallbackContext ctx)
     {
-        
+        vAxis = Mathf.Clamp(ctx.ReadValue<Vector2>().x, -1, 1);
+        hAxis = Mathf.Clamp(ctx.ReadValue<Vector2>().y, -1, 1);
+        Debug.Log($"XAxis : {vAxis} and Y Axis : {hAxis}");
     }
 
-    public void OnStart(InputAction.CallbackContext ctx)
+    public void OnJump(InputAction.CallbackContext ctx)
     {
+        if(!ctx.started) return;
+        Debug.Log("Press Jump");
+        rb.AddForce(0, jumpSpeed, 0, ForceMode.Force);
+    }
+    
+    public void OnStart(InputAction.CallbackContext ctx)
+    { 
         if (!isWaitingToHold) return;
         if (ctx.canceled) isHolding = false;
         else if (ctx.performed) isHolding = true;
