82492da7f1908824144b9e22a604a7641aab0dee
Mathis Jibert <mathis.jibert@gmail.com>
2023-10-04 15:03:55 +0200

    Anims

--- Flight/Assets/PlayerIdentity.cs
+++ Flight/Assets/PlayerIdentity.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class PlayerIdentity : MonoBehaviour
@@ -9,7 +10,43 @@ public class PlayerIdentity : MonoBehaviour
     public UIManager uiManager;
     public CameraController cameraController;
     public Transform visuals,headBone;
-    
+    public Animator animator;
+
+    public async void ChangeAnimation(Anim anim)
+    {
+        switch (anim)
+        {
+            case Anim.Glide:
+                animator.SetInteger("Animation",0);
+                break;
+            case Anim.Flap:
+                animator.SetInteger("Animation",1);
+                await Task.Delay(200);
+                animator.SetInteger("Animation",0);
+                break;
+            case Anim.Walk:
+                animator.SetInteger("Animation",4);
+                break;
+            case Anim.Idle:
+                animator.SetInteger("Animation",3);
+                break;
+            case Anim.Land:
+                animator.SetInteger("Animation",2);
+                await Task.Delay(200);
+                animator.SetInteger("Animation",3);
+                break;
+        }
+    }
 
 
+
+}
+
+public enum Anim
+{
+    Flap,
+    Glide,
+    Walk,
+    Idle,
+    Land
 }
